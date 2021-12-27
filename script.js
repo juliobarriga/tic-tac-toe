a96e9d0596212bfd93e12b130345856cf8b6d781
julio <juliodbarriga@gmail.com>
2021-12-27 16:33:39 -0600

    Created function to check wheter X or O is a winner and print it on the console.log.

--- script.js
+++ script.js
@@ -1,15 +1,61 @@
 let spaces = document.querySelectorAll('.space');
 let turn = 'X';
+
 spaces.forEach((element) => {
     element.innerHTML = "";
+    scores = {
+        X:[],
+        O:[]
+    }
+    let winner = "";
+    const checkWin = () =>{
+        // console.log(scores[turn]);
+        if (['a','d','g'].every(value => scores[turn].includes(value)) == true) {
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        } else if (['b','e','h'].every(value => scores[turn].includes(value)) == true) {
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        } else if (['c','f','i'].every(value => scores[turn].includes(value)) == true) {
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        } else if (['a','b','c'].every(value => scores[turn].includes(value)) == true) {
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        } else if (['d','e','f'].every(value => scores[turn].includes(value)) == true) {
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        } else if (['g','h','i'].every(value => scores[turn].includes(value)) == true) {
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        } else if (['a','e','i'].every(value => scores[turn].includes(value)) == true) {
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        } else if (['g','e','c'].every(value => scores[turn].includes(value)) == true){
+            winner = turn;
+            console.log(`The winner is ${winner}`);
+            return winner;
+        }
+    }
     element.addEventListener('click', ()=>{
     if (element.innerHTML.length == ""){
         if (turn === 'X'){
-        element.innerHTML = 'X';
-        turn = 'O';
+            scores[turn].push(element.id);
+            element.innerHTML = 'X';
+            if (scores[turn].length >= 3){checkWin()};
+            turn = 'O';
         } else if (turn === 'O'){
-        element.innerHTML = 'O';
-        turn = 'X'
+            scores[turn].push(element.id);
+            element.innerHTML = 'O';
+            if (scores[turn].length >= 3){checkWin()};
+            turn = 'X'
         }
     }
 });
@@ -20,3 +66,13 @@ const reset = () => {
     spaces.forEach(element => element.innerHTML = "");
 }
 resetBtn.addEventListener('click', reset)
+
+
+// console.log(['a','d','g'].every(value => scores[turn].includes(value)));
+// console.log(['b','e','h'].every(value => scores[turn].includes(value)));
+// console.log(['c','f','i'].every(value => scores[turn].includes(value)));
+// console.log(['a','b','c'].every(value => scores[turn].includes(value)));
+// console.log(['d','e','f'].every(value => scores[turn].includes(value)));
+// console.log(['g','h','i'].every(value => scores[turn].includes(value)));
+// console.log(['a','e','i'].every(value => scores[turn].includes(value)));
+// console.log(['g','e','c'].every(value => scores[turn].includes(value)));
