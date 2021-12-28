8f08191ae0408ae15c3d1f9e56e8cba598fddb54
julio <juliodbarriga@gmail.com>
2021-12-28 13:05:44 -0600

    Finished MVP with basic requirements.

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let spaces = document.querySelectorAll('.space');
 let turn = 'X';
+let winner = "";
 let scores = {
         X:[],
         O:[]
@@ -13,8 +14,9 @@ let oWins = () => {
     document.querySelector('.score-X').style.backgroundColor = 'red';
 }
 spaces.forEach((element) => {
+
     element.innerHTML = "";
-    let winner = "";
+    
     let scoreXElement = document.querySelector('div.score-X p.player-score');
     let scoreOElement = document.querySelector('div.score-O p.player-score');
 
@@ -22,130 +24,163 @@ spaces.forEach((element) => {
         // console.log(scores[turn]);
         if (['a','d','g'].every(value => scores[turn].includes(value)) == true) {
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#a').style.backgroundColor = 'green';
+                document.querySelector('#d').style.backgroundColor = 'green';
+                document.querySelector('#g').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#a').style.backgroundColor = 'green';
+                document.querySelector('#d').style.backgroundColor = 'green';
+                document.querySelector('#g').style.backgroundColor = 'green';
             } 
             return winner;
         } else if (['b','e','h'].every(value => scores[turn].includes(value)) == true) {
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#b').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#h').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#b').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#h').style.backgroundColor = 'green';
             } 
             return winner;
         } else if (['c','f','i'].every(value => scores[turn].includes(value)) == true) {
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#c').style.backgroundColor = 'green';
+                document.querySelector('#f').style.backgroundColor = 'green';
+                document.querySelector('#i').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#c').style.backgroundColor = 'green';
+                document.querySelector('#f').style.backgroundColor = 'green';
+                document.querySelector('#i').style.backgroundColor = 'green';
             } 
             return winner;
         } else if (['a','b','c'].every(value => scores[turn].includes(value)) == true) {
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#a').style.backgroundColor = 'green';
+                document.querySelector('#b').style.backgroundColor = 'green';
+                document.querySelector('#c').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#a').style.backgroundColor = 'green';
+                document.querySelector('#b').style.backgroundColor = 'green';
+                document.querySelector('#c').style.backgroundColor = 'green';
             } 
             return winner;
         } else if (['d','e','f'].every(value => scores[turn].includes(value)) == true) {
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#d').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#f').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#d').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#f').style.backgroundColor = 'green';
             } 
             return winner;
         } else if (['g','h','i'].every(value => scores[turn].includes(value)) == true) {
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#g').style.backgroundColor = 'green';
+                document.querySelector('#h').style.backgroundColor = 'green';
+                document.querySelector('#i').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#g').style.backgroundColor = 'green';
+                document.querySelector('#h').style.backgroundColor = 'green';
+                document.querySelector('#i').style.backgroundColor = 'green';
             } 
             return winner;
         } else if (['a','e','i'].every(value => scores[turn].includes(value)) == true) {
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#a').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#i').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#a').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#i').style.backgroundColor = 'green';
             } 
             return winner;
         } else if (['g','e','c'].every(value => scores[turn].includes(value)) == true){
             winner = turn;
-            console.log(`The winner is ${winner}`);
             if (winner == 'X') {
                 scoreXElement.innerHTML = parseInt(scoreXElement.innerHTML) + 1;
-                console.log(scoreXElement.innerHTML)
                 xWins();
+                document.querySelector('#g').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#c').style.backgroundColor = 'green';
             } else if (winner == 'O') {
                 scoreOElement.innerHTML = parseInt(scoreOElement.innerHTML) + 1;
                 oWins();
+                document.querySelector('#g').style.backgroundColor = 'green';
+                document.querySelector('#e').style.backgroundColor = 'green';
+                document.querySelector('#c').style.backgroundColor = 'green';
             } 
             return winner;
         }
     }
     element.addEventListener('click', ()=>{
-    if (element.innerHTML.length == ""){
-        if (turn === 'X'){
-            scores[turn].push(element.id);
-            element.innerHTML = 'X';
-            if (scores[turn].length >= 3){checkWin()};
-            turn = 'O';
-            if (winner.length == 0){
-                document.querySelector('.score-X').style.backgroundColor = 'white';
-                document.querySelector('.score-O').style.backgroundColor = 'yellow';
+        if (element.innerHTML.length == 0 && winner.length == 0){
+            if (turn === 'X'){
+                scores[turn].push(element.id);
+                element.innerHTML = 'X';
+                if (scores[turn].length >= 3){checkWin()};
+                turn = 'O';
+                if (winner.length == 0){
+                    document.querySelector('.score-X').style.backgroundColor = 'white';
+                    document.querySelector('.score-O').style.backgroundColor = 'yellow';
+                }
+                
+            } else if (turn === 'O'){
+                scores[turn].push(element.id);
+                element.innerHTML = 'O';
+                if (scores[turn].length >= 3){checkWin()};
+                turn = 'X'
+                if (winner.length == 0){
+                    document.querySelector('.score-O').style.backgroundColor = 'white';
+                    document.querySelector('.score-X').style.backgroundColor = 'yellow';
+                }
+                
             }
-            
-        } else if (turn === 'O'){
-            scores[turn].push(element.id);
-            element.innerHTML = 'O';
-            if (scores[turn].length >= 3){checkWin()};
-            turn = 'X'
-            if (winner.length == 0){
-                document.querySelector('.score-O').style.backgroundColor = 'white';
-                document.querySelector('.score-X').style.backgroundColor = 'yellow';
-            }
-            
         }
-    }
 });
 });
 const resetGameBtn = document.querySelector('.reset-game');
 const resetGame = () => {
+    document.querySelectorAll('.space').forEach(space => space.style.backgroundColor = 'white');
     document.querySelector('.score-O').style.backgroundColor = 'white';
     document.querySelector('.score-X').style.backgroundColor = 'yellow';
     turn = 'X';
@@ -163,13 +198,3 @@ const resetScores = () => {
     scoreOElement.innerHTML = 0;
 }
 resetScoresBtn.addEventListener('click', resetScores)
-
-
-// console.log(['a','d','g'].every(value => scores[turn].includes(value)));
-// console.log(['b','e','h'].every(value => scores[turn].includes(value)));
-// console.log(['c','f','i'].every(value => scores[turn].includes(value)));
-// console.log(['a','b','c'].every(value => scores[turn].includes(value)));
-// console.log(['d','e','f'].every(value => scores[turn].includes(value)));
-// console.log(['g','h','i'].every(value => scores[turn].includes(value)));
-// console.log(['a','e','i'].every(value => scores[turn].includes(value)));
-// console.log(['g','e','c'].every(value => scores[turn].includes(value)));
